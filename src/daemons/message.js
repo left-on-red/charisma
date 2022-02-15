5faacd2d56d6f2b1900368c1c009267deab78884
red <tachatz214@gmail.com>
2022-02-15 15:47:19 -0600

    fixed message listener

--- src/daemons/message.js
+++ src/daemons/message.js
@@ -1,23 +1,15 @@
 let parseMessage = require('./../parseMessage.js');
+let BotContext = require('./../core/BotContext.js');
 
-function is_bad(str) {
-    if (str.toLowerCase()
-        .replace(/0/g, 'o')
-        .replace(/3/g, 'e')
-        .includes('female dog')
-    ) { return true }
-}
-
-module.exports = function(imports) {
-    imports.messageListener = function() {
-        imports.client.on('messageCreate', function(message) {
-            if (is_bad(message.content)) { return message.delete() }
-            try { parseMessage(imports, message) }
-            catch(error) { console.error(error) }
-        });
-
-        imports.client.on('messageUpdate', function(oldMessage, newMessage) {
-            if (is_bad(newMessage.content)) { return newMessage.delete() }
+/**
+ * 
+ * @param {BotContext} context 
+ */
+module.exports = function(context) {
+    context.messageListener = function() {
+        context.client.on('messageCreate', function(message) {
+            try { parseMessage(context, message) }
+            catch(error) { context.logging.error(`guild.${message.guild.id}`, error) }
         });
     }
 }
