244a752205d926dc672a41eba067b298f0907863
red <tachatz214@gmail.com>
2022-02-15 22:39:34 -0600

    dither command more fine

--- src/commands/images/dither.js
+++ src/commands/images/dither.js
@@ -3,6 +3,13 @@ let ImageCommand = require('./../../core/templates/ImageCommand.js');
 module.exports = class extends ImageCommand {
     constructor() {
         super('dither', 'dither an image');
-        this.magick(data => { data.orderedDither('Red', '4x4').orderedDither('Green', '4x4').orderedDither('Blue', '4x4').orderedDither('Magenta', '4x4').orderedDither('Yellow', '4x4').orderedDither('Cyan', '4x4') });
+        this.magick(data => {
+            data
+                .orderedDither('Red', '2x2')
+                .orderedDither('Green', '2x2')
+                .orderedDither('Blue', '2x2')
+                .orderedDither('Magenta', '2x2')
+                .orderedDither('Yellow', '2x2')
+                .orderedDither('Cyan', '2x2') });
     }
 }
